544ce31305822f04a25aa2200683b9f4ac41a4f6
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2018-07-08 15:25:31 -0400

    deploy commit

--- client/src/actions/clientsActions.js
+++ client/src/actions/clientsActions.js
@@ -3,21 +3,14 @@ import { FETCH_CLIENTS, ADD_CLIENT, REMOVE_CLIENT, EDIT_CLIENT } from './types';
 
 export const getClients = () => async dispatch => {
   const res = await axios.get('/api/clients');
-  dispatch({type: FETCH_CLIENTS, payload: res.data})
-}
+  dispatch({ type: FETCH_CLIENTS, payload: res.data });
+};
 
 export const addClient = client => async dispatch => {
   const res = await axios.post('/api/client', client);
-  console.log("DATA RES", res.data);
-  dispatch({type: ADD_CLIENT, payload: res.data})
-}
-
-// export function addClient(client) {
-//   return {
-//     type: ADD_CLIENT,
-//     client,
-//   };
-// }
+  console.log('DATA RES', res.data);
+  dispatch({ type: ADD_CLIENT, payload: res.data });
+};
 
 export function removeClient(clientID) {
   return {
